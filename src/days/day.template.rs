884d73d1da96998a834ba11c60c1c4945502fa6b
Eugene Valty <eugenevalty@gmail.com>
2024-12-01 22:45:19 +0100

    Enhanced template

--- src/days/day.template.rs
+++ src/days/day.template.rs
@@ -1,9 +1,31 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+// The output is wrapped in a Result to allow matching on errors.
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn part1() -> String {
+    let lines = read_lines("inputs/dayn-1.txt").unwrap();
+    for line in lines {
+        // Do something...
+    }
     return String::from("part1");
 }
 
 fn part2() -> String {
-    return String::from("part2");
+    let lines = read_lines("inputs/dayn-1.txt").unwrap();
+    for line in lines {
+        // Do something...
+    }
+    return String::from("part1");
 }
 
 pub fn solve(part: i32) -> String {
