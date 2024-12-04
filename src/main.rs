2985dc853dd54ebafc6322edfb021d242565f3fb
evalty <eugene.valty.ext@louisvuitton.com>
2024-12-04 17:37:30 +0100

    Added possibility to give multiple day as CLI args

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use clap::Parser;
 #[derive(Parser)]
 struct Cli {
     /// The requested day to run
-    day: f32,
+    days: Vec<f32>,
 }
 
 fn get_day_solver(day: i32) -> fn(i32) -> i32 {
@@ -25,28 +25,30 @@ fn get_day_solver(day: i32) -> fn(i32) -> i32 {
 fn main() {
     let args: Cli = Cli::parse();
 
-    println!("Solving day #{}...", args.day);
+    for day in args.days {
+        println!("# Solving day #{}...", day);
 
-    // Retrieve day and part
-    let [day, part] = [
-        args.day.trunc() as i32,
-        (args.day.fract() * 10.0).round() as i32,
-    ];
+        // Retrieve day and part
+        let [day, part] = [day.trunc() as i32, (day.fract() * 10.0).round() as i32];
 
-    if part < 0 || part > 2 {
-        panic!("Incorrect part value ({}). Must be 1 or 2.", part)
+        if part < 0 || part > 2 {
+            panic!(" * Incorrect part value ({}). Must be 1 or 2.", part)
+        }
+
+        // Retrieve the request day
+        let problem: fn(i32) -> i32 = get_day_solver(day);
+
+        // Retrieve time
+        let start: Instant = Instant::now();
+
+        // Compute the solution
+        let solution: i32 = problem(part);
+
+        println!(" * Solution found: {}", solution);
+
+        println!(
+            " * Problem solved in {:.4?}s",
+            start.elapsed().as_secs_f64()
+        )
     }
-
-    // Retrieve the request day
-    let problem: fn(i32) -> i32 = get_day_solver(day);
-
-    // Retrieve time
-    let start: Instant = Instant::now();
-
-    // Compute the solution
-    let solution: i32 = problem(part);
-
-    println!("Solution found: {}", solution);
-
-    println!("Problem solved in {:.4?}s", start.elapsed().as_secs_f64())
 }
